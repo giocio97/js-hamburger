aec73acabc6b059aa69a5d147b44e77bb44ab40f
Giovanni Ciotta <giociotta@gmail.com>
2022-05-06 16:16:03 +0200

    inizio js

---
+++ main.js
@@ -0,0 +1,9 @@
+// mostrare / nascondere il menu principale (mobile)
+const bottoneHmenu = document.querySelector (" hamburger-menu");
+const bottoneAperto = document.querySelector (" fa-bars");
+const bottoneChiuso = document.querySelector (" fa-times");
+
+bottoneAperto.addEventListener(`click`),
+function() {
+
+}
